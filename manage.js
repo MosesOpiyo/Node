79379abd5e076c4e16d14db7fc097e7e7b495d08
MosesOpiyo <mosesopiyo54@gmail.com>
2023-11-09 15:32:14 +0300

    mongodb connection

--- manage.js
+++ manage.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const productRoutes = require('./api/routes/products')
 const app = express()
 
-
+mongoose.connect(
+    'mongodb+srv://moses:ix2Rlx3jN7VjdSOF@cluster0.qliuoq2.mongodb.net/'
+    )
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','*');
+    if(req.method === "OPTIONS"){
+        res.header('Access-Control-Allow-Methods','POST,PUT,PATCH,DELETE,GET');
+        return res.status(200).json({})
+    }
+    next()
+})
+
 app.use('/products', productRoutes)
 
 app.use((req,res,next)=>{
